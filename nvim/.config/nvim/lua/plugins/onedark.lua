567cb4cb8ff48ae1740082bfca23ab0f1773dac1
Kush Kansara <kkansara@tesla.com>
2023-04-20 00:09:35 -0400

    update colorschemes

--- nvim/.config/nvim/lua/plugins/onedark.lua
+++ nvim/.config/nvim/lua/plugins/onedark.lua
@@ -2,6 +2,10 @@ return {
   -- add gruvbox
   { "navarasu/onedark.nvim" },
 
+  { "ellisonleao/gruvbox.nvim" },
+
+  { "folke/tokyonight.nvim" },
+
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
