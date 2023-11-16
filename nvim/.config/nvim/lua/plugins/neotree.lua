1d1dcd96d85bebb13625e3f7dcde8a5db1f113cf
Kush Kansara <kkansara@tesla.com>
2023-11-16 08:51:20 -0500

    fix file tree stuff

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -12,4 +12,22 @@ return {
       },
     },
   },
+  keys = {
+    { "<leader>E", "<leader>fe", desc = "Explorer NeoTree (root dir)", remap = true },
+    { "<leader>e", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
+    {
+      "<leader>fE",
+      function()
+        require("neo-tree.command").execute({ toggle = true, dir = Util.root() })
+      end,
+      desc = "Explorer NeoTree (root dir)",
+    },
+    {
+      "<leader>fe",
+      function()
+        require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
+      end,
+      desc = "Explorer NeoTree (cwd)",
+    },
+  },
 }
