920a04740b6f4051469f0625c04dd8d8b3704ea6
Kush Kansara <kkansara@tesla.com>
2024-10-23 19:24:47 -0400

    oops update font size

--- wezterm/.config/wezterm/appearance.lua
+++ wezterm/.config/wezterm/appearance.lua
@@ -4,7 +4,7 @@ local module = {}
 function module.apply(config)
     -- config.color_scheme = "GruvboxDark"
     config.color_scheme = "OneDark (base16)"
-    config.font_size = 13
+    config.font_size = 16
 
     config.window_padding = {
         left = 0,
