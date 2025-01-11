c310879b3df2a68204812a6340942342c4673f7f
Kush Kansara <kkansara@tesla.com>
2025-01-11 11:07:13 -0500

    remove extra lsp keys

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -1,29 +1,6 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    init = function()
-      local keys = require("lazyvim.plugins.lsp.keymaps").get()
-
-      keys[#keys + 1] = { "<leader>cs", vim.diagnostic.setqflist, desc = "Show All Workspace Diagnostics" }
-
-      keys[#keys + 1] = {
-        "<leader>ce",
-        function()
-          vim.diagnostic.setqflist({ severity = "E" })
-        end,
-        desc = "Show All Workspace Errors",
-      }
-
-      keys[#keys + 1] = {
-        "<leader>cw",
-        function()
-          vim.diagnostic.setqflist({ severity = "W" })
-        end,
-        desc = "Show All Workspace Warnings",
-      }
-
-      keys[#keys + 1] = { "<leader>cb", vim.diagnostic.setloclist, desc = "Show Only Buffer Diagnostics" }
-    end,
     ---@class PluginLspOpts
     opts = {
       ---@type lspconfig.options
